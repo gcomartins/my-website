0d1e1181a81056fe4ab7a2b8e71fcaee13d8cdf1
conjone <gcomartins@gmail.com>
2024-11-25 18:38:32 -0300

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Configura o Next.js para gerar arquivos estáticos
+  basePath: '/<nome-do-repositorio>', // Substitua pelo nome do seu repositório
+  trailingSlash: true, // Adiciona barra no final dos caminhos
 };
 
-export default nextConfig;
+module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <p>Hey, there!</p>
-      <p>My name is Guilherme and this is my website!<br/>I'm a mobile developer, focused in Android, with 2 years of experience.</p>
+      <p>My name is Guilherme and this is my website!<br/>I am a mobile developer, focused in Android, with 2 years of experience.</p>
       <p>My main techonolgies are:</p>
       <ul className={styles.list}>
         <i>Android Kotlin/Java (2 years)</i>
