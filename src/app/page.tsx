23c049989ff010940130579d86d1fd0c7e616bff
conjone <gcomartins@gmail.com>
2024-11-25 20:32:08 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,29 @@
+import Head from "next/head";
 import styles from "./page.module.css";
+
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <p>Hey, there!</p>
-      <p>My name is Guilherme and this is my website!<br/>I am a mobile developer, focused in Android, with 2 years of experience.</p>
-      <p>My main techonolgies are:</p>
-      <ul className={styles.list}>
-        <i>Android Kotlin/Java (2 years)</i>
-        <i>React/React Native (1 year)</i>
-        <i>Javascript (1 year)</i>
-        <i>Flutter (1 and a half year)</i>
-      </ul>
-    </div>
-  );
+    <>
+      <Head>
+        <title>Hii</title>
+        <meta property='og:title' content={property.name} />
+        <meta property='og:description' content={property.description} />
+        <meta property='og:image' content={property.image} />
+        <meta property='og:url' content='https://gcomartins.github.io/my-website/' />
+        <meta property='og:type' content='website' />
+      </Head>
+      <div className={styles.page}>
+        <p>Hey, there!</p>
+        <p>My name is Guilherme and this is my website!<br />I am a mobile developer, focused in Android, with 2 years of experience.</p>
+        <p>My main techonolgies are:</p>
+        <ul className={styles.list}>
+          <i>Android Kotlin/Java (2 years)</i>
+          <i>React/React Native (1 year)</i>
+          <i>Javascript (1 year)</i>
+          <i>Flutter (1 and a half year)</i>
+        </ul>
+      </div>
+    </>
+  )
 }
