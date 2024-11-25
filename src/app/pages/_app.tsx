f4f8249a640e37f1bcef20dcbc7b580d7c6bef45
conjone <gcomartins@gmail.com>
2024-11-25 19:07:17 -0300

    Update preview links

--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -6,16 +6,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <Head>
-                {/* Meta tags Open Graph para prévias no WhatsApp */}
-                <meta property="og:title" content="See my website!" />
-                <meta
-                    property="og:description"
-                    content="Hi, there! My name is Guilherme and this is my website!"
-                />
-                <meta
-                    property="og:image"
-                    content="https://gcomartins.github.io/my-website/spike.png"
-                />
+                <title>Meu site incrível</title>
+                <meta property="og:title" content="Meu site incrível" />
+                <meta property="og:description" content="Descrição personalizada do meu site" />
+                <meta property="og:image" content="https://gcomartins.github.io/my-website/preview.png" />
+                <meta property="og:url" content="https://gcomartins.github.io/my-website/" />
                 <meta property="og:type" content="website" />
             </Head>
             <Component {...pageProps} />
