46298b9e80cb4a1d612c4b682eff625b260c897c
conjone <gcomartins@gmail.com>
2024-11-27 17:49:23 -0300

    Retirando react router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,6 @@
-"use client"
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Link from 'next/link';
 import HomePage from './pages/home/page';
-import ContactPage from './pages/contact/page';
-import ProjectsPage from './pages/projects/page';
 
-function App() {
-  const basePath = "/my-website"
-  return (
-    <Router>
-      <Routes>
-        <Route path={basePath} element={<HomePage />} />
-        <Route path={`${basePath}/contact`} element={<ContactPage />} />
-        <Route path={`${basePath}/projects`} element={<ProjectsPage />} />
-      </Routes>
-    </Router>
-  );
-}
+const App = () => HomePage()
 
-export default App;
+export default App
